13c58f3fd68a0c6a3b6d43ea543e85ca7fdf34b3
Jennie Robinson Faber <jennie@jenniefaber.com>
2022-02-28 11:04:53 -0500

    Update page layout

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,24 @@
 <template>
   <Layout>
     <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    We understand that finances may be strained. As a nonprofit community space,
+    Gamma relies 100% on member dues to cover our expenses. However, we don’t
+    want any member to feel hardship during this time, and your contributions as
+    a member have value beyond dues! If you need to pause your membership, you
+    can switch to the Free Community Member plan to stop your billing
+    immediately while keeping your membership active. You’ll still have access
+    to all of our community resources – it’s important that we remain together
+    through this! If circumstances allow you to continue contributing
+    financially, your support is greatly appreciated. We want there to be a
+    Gamma Space to come back to and your contributions will greatly help with
+    that.
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
-  }
-}
+    title: "About us",
+  },
+};
 </script>
